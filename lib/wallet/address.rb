4452e436bfd75be3667a020f780132b05a4d8b5a
Sam <ogminor@gmail.com>
2014-07-03 01:26:09 +0300

    Update address.rb

--- lib/wallet/address.rb
+++ lib/wallet/address.rb
@@ -2,22 +2,21 @@ module RubyWallet
   class Address
 
     attr_reader :account, :address
-    delegate :wallet, to: :account
 
     def initialize(account, address=nil)
       @address = if address
                    address
                  else
-                   wallet.getnewaddress(account.name)
+                   account.wallet.getnewaddress(account.name)
                  end
     end
 
     def total_received
-      wallet.getreceivedbyaddress(self.address, RubyWallet.config.min_conf)
+      account.wallet.getreceivedbyaddress(self.address, RubyWallet.config.min_conf)
     end
 
     def private_key
-      wallet.private_key(self.address)
+      account.wallet.private_key(self.address)
     end
 
     def ==(other_address)

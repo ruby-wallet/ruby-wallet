c31ffb35535cfb619b4f5fbe67445580dcb197df
Sam <ogminor@gmail.com>
2014-07-03 03:18:57 +0300

    Update dsl.rb
    Made it more coin neutral

--- lib/bitcoin/dsl.rb
+++ lib/bitcoin/dsl.rb
@@ -1,144 +1,145 @@
-module Bitcoin::DSL
-  def bitcoin
-    if self.class.respond_to?(:bitcoin)
-      @client ||= Bitcoin::Client.new(self.class.bitcoin.user, self.class.bitcoin.pass, self.class.bitcoin.options)
+module Coin::DSL
+  def coin
+    if self.class.respond_to?(:coin)
+      @client ||= Coin::Client.new(self.class.coin.user, self.class.coin.pass, self.class.coin.options)
     else
-      @client ||= Bitcoin::Client.new(nil, nil)
+      @client ||= Coin::Client.new(nil, nil)
     end
   end
   
   def username=(value)
-    bitcoin.user = value
+    coin.user = value
   end
   
   def password=(value)
-    bitcoin.pass = value
+    coin.pass = value
   end
   
   def host=(value)
-    bitcoin.host = value
+    coin.host = value
   end
   
   def port=(value)
-    bitcoin.port = value
+    coin.port = value
   end
   
   def ssl=(value)
-    bitcoin.ssl = value
+    coin.ssl = value
   end
   
   def username(value = nil)
-    value ? bitcoin.user = value : bitcoin.user
+    value ? coin.user = value : coin.user
   end
   
   def password(value = nil)
-    value ? bitcoin.pass = value : bitcoin.pass
+    value ? coin.pass = value : coin.pass
   end
   
   def host(value = nil)
-    value ? bitcoin.host = value : bitcoin.host
+    value ? coin.host = value : coin.host
   end
   
   def port(value = nil)
-    value ? bitcoin.port = value : bitcoin.port
+    value ? coin.port = value : coin.port
   end
   
   def ssl(value = nil)
-    value.nil? ? bitcoin.ssl : bitcoin.ssl = value
+    value.nil? ? coin.ssl : coin.ssl = value
   end
   
   def ssl?
-    bitcoin.ssl?
+    coin.ssl?
   end
   
   
   # Safely copies wallet.dat to destination, which can be a directory or a path with filename. 
+  # Should add options to compress it and send it to a remote server
   def backupwallet(destination)
-    bitcoin.backupwallet destination
+    coin.backupwallet destination
   end
   
   # Returns the account associated with the given address. 
   def getaccount(bitcoinaddress)
-    bitcoin.getaccount bitcoinaddress
+    coin.getaccount bitcoinaddress
   end
   
-  # Returns the current bitcoin address for receiving payments to this account. 
+  # Returns the current coin address for receiving payments to this account. 
   def getaccountaddress(account)
-    bitcoin.getaccountaddress account
+    coin.getaccountaddress account
   end
   
   #	Returns the list of addresses for the given account. 
   def getaddressesbyaccount(account)
-    bitcoin.getaddressesbyaccount account
+    coin.getaddressesbyaccount account
   end
   
   # If +account+ is not specified, returns the server's total available balance.
   # If +account+ is specified, returns the balance in the account.
   def getbalance(account = nil, minconf = 1)
-    bitcoin.getbalance account, minconf
+    coin.getbalance account, minconf
   end
   
   # Dumps the block existing at specified height.
   # Note: this is not available in the official release
   def getblockbycount(height)
-    bitcoin.getblockbycount height
+    coin.getblockbycount height
   end
   
   # Returns the number of blocks in the longest block chain.
   def getblockcount
-    bitcoin.getblockcount
+    coin.getblockcount
   end
   
   # Returns the block number of the latest block in the longest block chain. 
   def getblocknumber
-    bitcoin.getblocknumber
+    coin.getblocknumber
   end
   
   # Returns the number of connections to other nodes.
   def getconnectioncount
-    bitcoin.getconnectioncount
+    coin.getconnectioncount
   end
   
   # Returns the proof-of-work difficulty as a multiple of the minimum difficulty. 
   def getdifficulty
-    bitcoin.getdifficulty
+    coin.getdifficulty
   end
   
   # Returns true or false whether bitcoind is currently generating hashes 
   def getgenerate
-    bitcoin.getgenerate
+    coin.getgenerate
   end
   
   # Returns a recent hashes per second performance measurement while generating. 
   def gethashespersec
-    bitcoin.gethashespersec
+    coin.gethashespersec
   end
 
   # Returns an object containing various state info. 
   def getinfo
-    bitcoin.getinfo
+    coin.getinfo
   end
     
-  # Returns a new bitcoin address for receiving payments. If +account+ is specified (recommended),
+  # Returns a new coin address for receiving payments. If +account+ is specified (recommended),
   # it is added to the address book so payments received with the address will be credited to +account+.
   def getnewaddress(account = nil)
-    bitcoin.getnewaddress account
+    coin.getnewaddress account
   end
   
   # Returns the total amount received by addresses with +account+ in transactions
   # with at least +minconf+ confirmations. 
   def getreceivedbyaccount(account, minconf = 1)
-    bitcoin.getreceivedbyaccount account, minconf
+    coin.getreceivedbyaccount account, minconf
   end
   
-  # Returns the total amount received by +bitcoinaddress+ in transactions with at least +minconf+ confirmations. 
-  def getreceivedbyaddress(bitcoinaddress, minconf = 1)
-    bitcoin.getreceivedbyaddress bitcoinaddress, minconf
+  # Returns the total amount received by +coinaddress+ in transactions with at least +minconf+ confirmations. 
+  def getreceivedbyaddress(coinaddress, minconf = 1)
+    coin.getreceivedbyaddress coinaddress, minconf
   end
   
   # Get detailed information about +txid+ 
   def gettransaction(txid)
-    bitcoin.gettransaction txid
+    coin.gettransaction txid
   end
   
   # If +data+ is not specified, returns formatted hash data to work on:
@@ -150,17 +151,17 @@ module Bitcoin::DSL
   #
   # If +data+ is specified, tries to solve the block and returns true if it was successful.
   def getwork(data = nil)
-    bitcoin.getwork data
+    coin.getwork data
   end
   
   # List commands, or get help for a command. 
   def help(command = nil)
-    bitcoin.help command
+    coin.help command
   end
   
   # Returns Object that has account names as keys, account balances as values. 
   def listaccounts(minconf = 1)
-    bitcoin.listaccounts minconf
+    coin.listaccounts minconf
   end
   
   # Returns an array of objects containing:
@@ -170,7 +171,7 @@ module Bitcoin::DSL
   #   :confirmations => number of confirmations of the most recent transaction included
   #
   def listreceivedbyaccount(minconf = 1, includeempty = false)
-    bitcoin.listreceivedbyaccount minconf, includeempty
+    coin.listreceivedbyaccount minconf, includeempty
   end
   
   # Returns an array of objects containing:
@@ -180,66 +181,66 @@ module Bitcoin::DSL
   #   :amount        => total amount received by the address
   #   :confirmations => number of confirmations of the most recent transaction included 
   # 
-  # To get a list of accounts on the system, execute bitcoind listreceivedbyaddress 0 true
+  # To get a list of accounts on the system, execute coind listreceivedbyaddress 0 true
   def listreceivedbyaddress(minconf = 1, includeempty = false)
-    bitcoin.listreceivedbyaddress minconf, includeempty
+    coin.listreceivedbyaddress minconf, includeempty
   end
   
   # Returns up to +count+ most recent transactions for account +account+. 
   def listtransactions(account, count = 10)
-    bitcoin.listtransactions account, count
+    coin.listtransactions account, count
   end
   
   # Move from one account in your wallet to another. 
   def move(fromaccount, toaccount, amount, minconf = 1, comment = nil)
-    bitcoin.move fromaccount, toaccount, amount, minconf, comment
+    coin.move fromaccount, toaccount, amount, minconf, comment
   end
   
   # +amount+ is a real and is rounded to 8 decimal places. Returns the transaction ID if successful. 
-  def sendfrom(fromaccount, tobitcoinaddress, amount, minconf = 1, comment = nil, comment_to = nil)
-    bitcoin.sendfrom fromaccount, tobitcoinaddress, amount, minconf, comment, comment_to
+  def sendfrom(fromaccount, tobcoinaddress, amount, minconf = 1, comment = nil, comment_to = nil)
+    coin.sendfrom fromaccount, tocoinaddress, amount, minconf, comment, comment_to
   end
   
   # +amount+ is a real and is rounded to 8 decimal places 
-  def sendtoaddress(bitcoinaddress, amount, comment = nil, comment_to = nil)
-    bitcoin.sendtoaddress bitcoinaddress, amount, comment, comment_to
+  def sendtoaddress(coinaddress, amount, comment = nil, comment_to = nil)
+    coin.sendtoaddress coinaddress, amount, comment, comment_to
   end
   
   # Sets the account associated with the given address. 
-  def setaccount(bitcoinaddress, account)
-    bitcoin.setaccount bitcoinaddress, account
+  def setaccount(coinaddress, account)
+    coin.setaccount coinaddress, account
   end
   
   # +generate+ is true or false to turn generation on or off.
   # Generation is limited to +genproclimit+ processors, -1 is unlimited.
   def setgenerate(generate, genproclimit = -1)
-    bitcoin.setgenerate generate, genproclimit
+    coin.setgenerate generate, genproclimit
   end
   
   # Stop bitcoin server. 
   def stop
-    bitcoin.stop
+    coin.stop
   end
   
-  # Return information about +bitcoinaddress+. 
-  def validateaddress(bitcoinaddress)
-    bitcoin.validateaddress
+  # Return information about +coinaddress+. 
+  def validateaddress(coinaddress)
+    coin.validateaddress
   end
 
   def encryptwallet(passphrase)
-    bitcoin.encryptwallet passphrase
+    coin.encryptwallet passphrase
   end
 
   def walletpassphrase(passphrase, timeout = 20)
-    bitcoin.walletpassphrase passphrase, 20
+    coin.walletpassphrase passphrase, 20
   end
 
   def walletlock
-    bitcoin.walletlock
+    coin.walletlock
   end
 
-  def dumpprivkey(address)
-    bitcoin.dumpprivkey address
+  def dumpprivkey(coinaddress)
+    coin.dumpprivkey coinaddress
   end
 
   alias encrypt encryptwallet

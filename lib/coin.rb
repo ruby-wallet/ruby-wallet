aca7fca9fe3dd44e1b7a73c8a6974593eeb893b3
Sam <ogminor@gmail.com>
2014-07-03 03:03:58 +0300

    Update and rename bitcoin.rb to coin.rb
    Making the project coin neutral

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,26 +1,25 @@
-module Bitcoin
-  autoload :Client, 'bitcoin/client'
-  autoload :API,    'bitcoin/api'
-  autoload :Request,'bitcoin/request'
-  autoload :RPC,    'bitcoin/rpc'
-  autoload :Errors, 'bitcoin/errors'
-  autoload :Version,'bitcoin/version'
-  autoload :VERSION,'bitcoin/version'
-  autoload :DSL,    'bitcoin/dsl'
+module Coin
+  autoload :Client, 'coin/client'
+  autoload :API,    'coin/api'
+  autoload :Request,'coin/request'
+  autoload :RPC,    'coin/rpc'
+  autoload :Errors, 'coin/errors'
+  autoload :Version,'coin/version'
+  autoload :VERSION,'coin/version'
+  autoload :DSL,    'coin/dsl'
   
   def self.included(base)
-    base.send(:include, Bitcoin::DSL)
-    base.send(:extend,  Bitcoin::DSL)
+    base.send(:include, Coin::DSL)
+    base.send(:extend,  Coin::DSL)
   end
 end
 
-def Bitcoin(user, pass, options = {})
-  ::Bitcoin::Client.new(user, pass, options)
+def Coin(user, pass, options = {})
+  ::Coin::Client.new(user, pass, options)
 end
 
 require 'ostruct'
 require 'active_support/core_ext/module/attribute_accessors'
-require 'active_support/core_ext/module/delegation'
 require 'active_support/core_ext/hash/indifferent_access'
 require 'active_support/core_ext/string'
 

bc92e21a538a48670e1345337e90cf4bea7d36c6
Xian <admin@blackwavelabs.com>
2014-07-25 21:32:06 +0300

    Update version.rb

--- lib/coind/version.rb
+++ lib/coind/version.rb
@@ -2,7 +2,7 @@ module Coind
   module Version
     MAJOR = 0
     MINOR = 2
-    PATCH = 1
+    PATCH = 2
     REL = nil
     STRING = REL ? [MAJOR, MINOR, PATCH, REL].join('.') : [MAJOR, MINOR, PATCH].join('.')
   end

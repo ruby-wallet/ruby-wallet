226709436d1774789c87e7d018819293b6440ee3
drunkonsound <admin@blackwavelabs.com>
2014-07-29 09:48:40 -0400

    Updated to version 0.5

--- lib/version.rb
+++ lib/version.rb
@@ -1,8 +1,8 @@
 module RubyWallet
   module Version
     MAJOR = 0
-    MINOR = 4
-    PATCH = 4
+    MINOR = 5
+    PATCH = 0
     STRING =[MAJOR, MINOR, PATCH].join('.')
   end
   VERSION = Version::STRING
